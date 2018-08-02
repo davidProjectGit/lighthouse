67396b796347862e059991bb72b1eb8f5e6f8569
Paul Irish <paulirish@google.com>
2018-08-02 12:16:47 -0700

    core(i18n): expose locale on lhr.i18n

--- typings/lhr.d.ts
+++ typings/lhr.d.ts
@@ -46,7 +46,7 @@ declare global {
       /** Execution timings for the Lighthouse run */
       timing: {total: number, [t: string]: number};
       /** The record of all formatted string locations in the LHR and their corresponding source values. */
-      i18n?: {rendererFormattedStrings: I18NRendererStrings, icuMessagePaths: I18NMessages};
+      i18n?: {rendererFormattedStrings: I18NRendererStrings, icuMessagePaths: I18NMessages, locale: Locale};
     }
 
     // Result namespace
